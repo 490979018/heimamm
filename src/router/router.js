23aa21f100f1bb2662006e9377329b3bdd3e19a4
陈梓煌 <490979018@qq.com>
2020-04-05 11:36:08 +0800

    路由整合

---
+++ src/router/router.js
@@ -0,0 +1,10 @@
+import Vue from 'vue';
+import vueRouter from 'vue-router';
+Vue.use(vueRouter);
+const router= new vueRouter({
+    routes:[
+        {path:'/',component:null}
+    ]
+});
+
+export default router;
